22e51bffcc1b74d33525d3e606e4450d26c47db3
lokeshkumardev <lokesh.kumar@webappvertex.com>
2025-02-20 15:26:58 +0530

    save cert file

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,17 @@ import { WsAdapter } from '@nestjs/platform-ws';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
 dotenv.config({ path: './.env' });
+import * as fs from 'fs';
+import * as path from 'path';
+
 async function bootstrap() {
+  const keyPath = path.join(__dirname, '../server.key');
+  const certPath = path.join(__dirname, '../server.crt');
+
+  const httpsOptions = {
+    key: fs.readFileSync(keyPath),
+    cert: fs.readFileSync(certPath),
+  };
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api/v1');
