6ee95ee6e3a12712c48cb661a27d6ee3ee284aba
lokeshkumardev <lokesh.kumar@webappvertex.com>
2025-02-20 13:57:25 +0530

    change in https in main ts

--- src/main.ts
+++ src/main.ts
@@ -4,12 +4,21 @@ import { WsAdapter } from '@nestjs/platform-ws';
 import { ValidationPipe } from '@nestjs/common';
 import * as dotenv from 'dotenv';
 dotenv.config({ path: './.env' });
+const fs = require('fs');
+const path = require('path');
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  
+  const httpsOptions = {
+    key: fs.readFileSync(path.join(__dirname, '../ssl/server.key')),
+    cert: fs.readFileSync(path.join(__dirname, '../ssl/server.crt')),
+  };
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.enableCors();
   app.setGlobalPrefix('api/v1');
   // app.useWebSocketAdapter(new WsAdapter(app));
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT as string);
 }
+
 bootstrap();
